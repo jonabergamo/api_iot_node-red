0598c80eb6fb1432d312f22dbe3b619b3f950074
jonabergamo <jonathanbergamo16@gmail.com>
2024-03-13 22:08:52 -0300

    refactor: remove user and auth

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,15 +1,17 @@
-import { Controller, Get, Render } from "@nestjs/common";
-import { DataService } from "./data/data.service";
-import { Data } from "./schemas/data.schema";
+import { Controller, Get, Render } from '@nestjs/common';
+import { DataService } from './data/data.service';
+import { Data } from './schemas/data.schema';
 
 @Controller()
 export class AppController {
   constructor(private readonly dataService: DataService) {}
 
   @Get()
-  @Render("index")
+  @Render('index')
   async getHello(): Promise<object> {
-    const lastData = (await this.dataService.getLast()) as Data & { createdAt: Date };
+    const lastData = (await this.dataService.getLast()) as Data & {
+      createdAt: Date;
+    };
     const allData = (await this.dataService.getAll()).sort();
     return {
       Sensor: lastData.sensor,
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
-import { Module } from "@nestjs/common";
-import { MongooseModule } from "@nestjs/mongoose";
-import { usersModule } from "./users/users.module";
-import { AppController } from "./app.controller";
-import { DataModule } from "./data/data.module";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { AppController } from './app.controller';
+import { DataModule } from './data/data.module';
 
 @Module({
-  imports: [MongooseModule.forRoot("mongodb://localhost:27017/nestjs_teste"), usersModule, DataModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/nestjs_teste'),
+    DataModule,
+  ],
   controllers: [AppController],
   providers: [],
 })
